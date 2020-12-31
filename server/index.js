15a0f56b42b5599b88b95294784ff212a73497ab
Stephen <xerananymous@gmail.com>
2020-12-31 17:06:05 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
 var request = require('request');
+var cors = require('cors')
 
+app.use(cors())
 app.get("/get", (req, res) => {
     var options = {
         'method': 'GET',
         'url': 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-11-30&sortBy=publishedAt&apiKey=9bd313e14c144e888df672a34a0b29a0',
+        
       };
       request(options, function (error, response) {
         if (error) throw new Error(error);
+        
         res.send(response.body);
       });
 });
