f64f470431cb8dc3f1559fbcc3c88c32803fa353
Anurag <aknrg627@gmail.com>
2021-01-07 16:07:58 +0530

    added dashboard

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,16 +8,24 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800'},
+      { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css'}
     ]
   },
 
+  loading: { color: '#fff' },
+
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
+    'assets/css/demo.css',
+    'assets/css/nucleo-icons.css',
+    'assets/sass/black-dashboard.scss'
   ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    `~/plugins/dashboard-plugin.js`
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -27,13 +35,31 @@ export default {
   buildModules: [
   ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
+    '@nuxtjs/pwa',
+    'nuxt-i18n'
   ],
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        file: 'en.js'
+      },
+      {
+        code: 'ar',
+        file: 'ar.js'
+      }
+    ],
+    lazy: true,
+    langDir: 'lang/',
+    defaultLocale: 'en',
   },
+  /*
+  ** Build configuration
+  */
+  build: {
+    },
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
   server:{
     port:8080,
     host: '0.0.0.0'
