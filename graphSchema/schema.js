58a9661e623178415b1c974f0553460b40dff779
stephenraj22 <63413057+stephenraj22@users.noreply.github.com>
2021-01-11 00:38:01 +0530

    Update schema.js

--- graphSchema/schema.js
+++ graphSchema/schema.js
@@ -404,24 +404,18 @@ const RootMutationType = new GraphQLObjectType({
         }
       },
       candidateResponses:{
-        type:GraphQLString,
+        type:ResponseType,
         args:{
           candidateId:{type:GraphQLNonNull(GraphQLID)}
         },
         resolve:async (parent,args)=>{
-          var val=await runQuery(args.candidateId);
-          console.log(val)
+          Response.find({candidateId:args.candidateId})
           
         }
       }
     })
   })
-async function runQuery(a){
-  Response.find({candidateId:a},(err,docs)=>{
-    console.log(docs)
-    return docs.candidateName
-  })
-}
+
  module.exports = new GraphQLSchema({
     query:RootQueryType,
     mutation: RootMutationType
