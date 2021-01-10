ec9dbac0eac776c05e3218c76d30c2fa627f2016
Aniket Chougule <acc439467@gmail.com>
2021-01-10 20:02:06 +0530

    Updated Frontend

---
+++ pages/user.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row">
+    <div class="col-md-8"><edit-profile-form> </edit-profile-form></div>
+    <div class="col-md-4"><user-card> </user-card></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "user",
+  components: {},
+};
+</script>
+<style></style>
