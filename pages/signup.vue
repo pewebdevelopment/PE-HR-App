620881ab471952b435aa43dfd2273991263b7a90
Anurag <aknrg627@gmail.com>
2021-02-02 01:19:57 +0530

    added some models in form

--- pages/signup.vue
+++ pages/signup.vue
@@ -8,18 +8,18 @@
   <form>
     <p class="h2 text-center mb-4">Sign up</p>
     <label for="defaultFormRegisterNameEx" class="grey-text">Your name</label>
-    <input type="text" id=" " class="form-control"/>
+    <input type="text" id=" " v-model = "input.name" class="form-control"/>
     <br/>
     <label for="defaultFormRegisterEmailEx" class="grey-text">Your email</label>
-    <input type="email" id=" " class="form-control"/>
+    <input type="email" id=" " v-model = "input.email" class="form-control"/>
     <br/>
     <label for="defaultFormRegisterConfirmEx" class="grey-text">Confirm your email</label>
-    <input type="email" id=" " class="form-control"/>
+    <input type="password" id=" " v-model = "input.cnfpassword" class="form-control"/>
     <br/>
     <label for="defaultFormRegisterPasswordEx" class="grey-text">Your password</label>
-    <input type="password" id=" " class="form-control"/>
+    <input type="password" id=" " v-model = "input.password" class="form-control"/>
     <div class="text-center mt-4">
-      <button class="btn btn-unique" type="submit">Register</button>
+      <button class="btn btn-unique" @click = "register" type="submit">Register</button>
     </div>
   </form>
 
@@ -32,9 +32,24 @@
 <script>
   
   export default {
+    data(){
+      return {
+        input :{
+          name : "",
+          email : "",
+          cnfpassword : "",
+          password : ""
+        }
+      };
+    },
     name: 'signup',
     components: {
      
+    },
+    methods : {
+      register(){
+        
+      }
     }
   }
 </script>
