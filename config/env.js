90cb658a62e1e4d043472fa199208c5d13f42104
Stephen <xerananymous@gmail.com>
2021-02-03 22:32:39 +0530

    new pool

--- config/env.js
+++ config/env.js
@@ -3,8 +3,11 @@ config = {
         connectionString:"mongodb+srv://Admin:Admin@cluster0.zxwa5.mongodb.net/test"
     },
     PORT:5000,
-    pool_region :'us-east-2',
+    /*pool_region :'us-east-2',
     UserPoolId : "us-east-2_gbGsTviN1",    
-    ClientId : "5mancm4soeolgeii6aa01p6lm4",
+    ClientId : "5mancm4soeolgeii6aa01p6lm4",*/
+    pool_region :'ap-south-1',
+    UserPoolId : "ap-south-1_n75iYQRxh",    
+    ClientId : "78hg39ucp4rprhrgp0bes9nuu8",
 }
 module.exports = config
